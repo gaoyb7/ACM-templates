c804109775facc947f6896f3408439c63bff599a
gaoyb7 <gaoyb7@gmail.com>
2015-02-26 15:33:21 +0800

    update

--- halfPlaneInter.cpp
+++ halfPlaneInter.cpp
@@ -1,25 +1,24 @@
 bool onleft(point p, line hp) {
-    return dcmp(cross(hp.s, hp.t, p)) >= 0;
+    return dcmp(cross(hp.s, hp.t, p)) > 0;          // >=
 }
 
 bool cmpang(line a, line b) {
     return dcmp(a.ang - b.ang) ? a.ang < b.ang : cross(a.s, a.t, b.s) < 0;
 }
 
-void hpinter(line *q, int n, point *h, int &m) {
-    int head = 0, tail = 0;
-    m = 0;
-    sort(q, q + n, cmpang);
+void hpinter(line *L, int n, point *p, int &m) {
+    int head = 0, tail = 0; m = 0;
+    sort(L, L + n, cmpang);
     for (int i = 1; i < n; ++i) {
-        if (!dcmp(q[i].ang - q[i - 1].ang)) continue;
-        while (head < tail && !onleft(h[tail - 1], q[i])) --tail;
-        while (head < tail && !onleft(h[head], q[i])) ++head;
-        q[++tail] = q[i];
-        if (head < tail) inter(q[tail - 1], q[tail], h[tail - 1]);
+        if (!dcmp(L[i].ang - L[i - 1].ang)) continue;
+        while (head < tail && !onleft(p[tail - 1], L[i])) --tail;
+        while (head < tail && !onleft(p[head], L[i])) ++head;
+        L[++tail] = L[i];
+        if (head < tail) inter(L[tail - 1], L[tail], p[tail - 1]);
     }
-    while (head < tail && !onleft(h[tail - 1], q[head])) --tail;
-    if (head < tail) inter(q[tail], q[head], h[tail]);
-    for (int i = head; i <= tail; ++i) h[m++] = h[i];
+    while (head < tail && !onleft(p[tail - 1], L[head])) --tail;
+    if (head < tail) inter(L[tail], L[head], p[tail]);
+    for (int i = head; i <= tail; ++i) p[m++] = p[i];
 }
 
 line makehp(double a, double b, double c) {     //ax + by + c > 0
