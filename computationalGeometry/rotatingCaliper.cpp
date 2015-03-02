b3aca85cd61fb36ca245c772b4080bbb39ec62e1
gaoyb7 <gaoyb7@gmail.com>
2015-03-02 14:02:25 +0800

    update

--- computationalGeometry/rotatingCaliper.cpp
+++ computationalGeometry/rotatingCaliper.cpp
@@ -1,30 +1,50 @@
-//Farthest Pair
+//farthest pair
 double maxdis(point *p, int n) {
     int m;
     andrew(p, n, h, m);
-    if (m == 2) return abs(h[0] - h[1]);
+    if (m == 2) return dis(h[0], h[1]);
     h[m] = h[0];
-    double d = 0;
-    for (int i = 0, j = 1; i < m; ++i) {
-        while (dcmp(cross(h[i + 1] - h[i], h[j + 1] - h[j])) > 0)
-            j = (j + 1) % m;
-        d = max(d, abs(h[i] - h[j]));
-    }
-    return d;
-}
-
-double maxdis(point *p, int n) {
-    int m;
-    andrew(p, n, h, m);
-    if (m == 2) return abs(h[0], h[1]);
-    h[m] = h[0];
-    abs ans = 0;
+    double ans = 0;
     for (int p = 0, q = 1; p < m; ++p) {
         while (cross(h[p], h[p + 1], h[q]) < cross(h[p], h[p + 1], h[q + 1]))
             q = (q + 1) % m;
-        ans = max(ans, max(abs(h[p], h[q]), abs(h[p + 1], h[q])));
+        ans = max(ans, max(dis(h[p], h[q]), dis(h[p + 1], h[q])));
+    }
+    return ans;
+}
+
+//mininal distance between convec hull
+double mindis(point *p1, int n, point *p2 , int m) {
+    int i = 0, j = 0;
+    ans = inf;
+    for (int k = 1; k < n; ++k) if (cmpx(p1[k], p1[i])) i = k ;
+    for (int k = 1; k < m; ++k) if (cmpx(p2[j], p2[k])) j = k ;
+    for (int t = n + n; t--;) {
+        if (dcmp(cross(p1[i + 1] - p1[i] , p2[j + 1] - p2[j])) < 0) {
+            ans = min(ans, distoseg(p2[j], line(p1[i], p1[i + 1])));
+            i = (i + 1) % n ;
+        } else {
+            ans = min(ans, distoseg(p1[i], line(p2[j], p2[j + 1])));
+            j = (j + 1) % m ;
+        }
     }
     return ans;
 }
 
 
+//minimal rectangle
+void minrec(point *p, int n, double &A, double &P) {
+    andrew(p, n, h, m);
+    int l = 1, r = 1, u = 1;
+    A = P = inf; h[m] = h[0];
+    for (int i = 0; i < m; ++i) {
+        vec edge = unit(h[i + 1] - h[i]);
+        while (dot(edge, h[r % m] - h[i]) < dot(edge, h[(r + 1) % m] - h[i])) ++r;
+        while (u < r || cross(edge, h[u % m] - h[i]) < cross(edge, h[(u + 1) % m] - h[i])) ++u;
+        while (l < u || dot(edge, h[l % m] - h[i]) > dot(edge, h[(l + 1) % m] - h[i])) ++l;
+        double W = dot(edge, h[r % m] - h[i]) - dot(edge, h[l % m] - h[i]);
+        double H = distoline(h[u % m], line(h[i + 1], h[i]));
+        A = min(A, W * H);
+        P = min(P, 2 * (W + H));
+    }
+}
