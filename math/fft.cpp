390784d476b4fcb85f35a5c76a3d12efc11d67ec
gaoyb7 <gaoyb7@gmail.com>
2015-08-14 21:44:08 +0800

    update

--- math/fft.cpp
+++ math/fft.cpp
@@ -1,5 +1,5 @@
 void fft(cp *a, int n, int f) {
-    static cp b[maxn];
+    static cp tmp[maxn];
     double arg = pi;
     for (int k = n >> 1; k; k >>= 1, arg *= 0.5) {
         cp wm = polar(1.0, f * arg), w(1, 0);
@@ -7,15 +7,13 @@ void fft(cp *a, int n, int f) {
             int p = i << 1;
             if (p >= n) p -= n;
             for (int j = 0; j < k; ++j)
-                b[i + j] = a[p + j] + w * a[p + k + j];
+                tmp[i + j] = a[p + j] + w * a[p + k + j];
         }
-        for (int i = 0; i < n; ++i) a[i] = b[i];
+        for (int i = 0; i < n; ++i) a[i] = tmp[i];
     }
-    /*
     if (f == -1)
         for (int i = 0; i < n; ++i)
             a[i] /= n;
-    */
 }
 
 // fft(a, n, 1); --- dft
