7fb34e041eb6a9db02e53b984fe3df3d7c90d8d8
gaoyb7 <gaoyb7@gmail.com>
2015-09-09 11:17:51 +0800

    update

--- graph/isap.cpp
+++ graph/isap.cpp
@@ -1,16 +1,16 @@
 struct edge { int v, f, nxt; };
 edge e[maxn * maxn];
 int g[maxn], h[maxn], vh[maxn];
-int psz, esz, S, T;
+int esz, vsz, S, T;
 
 void init(int n) {
-    psz = 1; esz = n;
+    esz = 1; vsz = n;
     memset(g, 0, sizeof(g));
 }
 
-void adde_dge(int u, int v, int f) {
-    e[++psz] = (edge){v, f, g[u]}; g[u] = psz;
-    e[++psz] = (edge){u, 0, g[v]}; g[v] = psz;
+void add_edge(int u, int v, int f) {
+    e[++esz] = (edge){v, f, g[u]}; g[u] = esz;
+    e[++esz] = (edge){u, 0, g[v]}; g[v] = esz;
 }
 
 int aug(int u, int f) {
@@ -21,24 +21,24 @@ int aug(int u, int f) {
         if (e[i].f && h[u] == h[v] + 1) {
             int ff = aug(v, min(e[i].f, d));
             e[i].f -= ff; e[i ^ 1].f += ff; d -= ff;
-            if (h[S] == esz || !d) return f - d;
+            if (h[S] == vsz || !d) return f - d;
         }
     }
-    int w = d < f ? min(esz, h[u] + 2) : esz;
+    int w = d < f ? min(vsz, h[u] + 2) : vsz;
     for (int i = g[u]; i; i = e[i].nxt)
         if (e[i].f)
             w = min(w, h[e[i].v] + 1);
     ++vh[w];
-    --vh[h[u]] ? h[u] = w : h[S] = esz;
+    --vh[h[u]] ? h[u] = w : h[S] = vsz;
     return f - d;
 }
 
 int max_flow() {
     memset(h, 0, sizeof(h));
     memset(vh, 0, sizeof(vh));
-    vh[0] = esz;
+    vh[0] = vsz;
     int ret = 0;
-    while (h[S] != esz)
+    while (h[S] != vsz)
         ret += aug(S, inf);
     return ret;
 }
