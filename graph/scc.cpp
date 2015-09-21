1c7ecba7c6e5e78a7565d00cf9767c3d7f47c3c4
gaoyb7 <gaoyb7@gmail.com>
2015-09-21 22:13:08 +0800

    update

---
+++ graph/scc.cpp
@@ -0,0 +1,34 @@
+int pre[maxn], low[maxn], sccno[maxn], dfs_clock, scc_cnt;
+vector<int> e[maxn];
+stack<int> S;
+
+void dfs(int u) {
+    pre[u] = low[u] = ++dfs_clock;
+    S.push(u);
+    for (size_t i = 0; i < e[u].size(); ++i) {
+        int v = e[u][i];
+        if (!pre[v]) {
+            dfs(v);
+            low[u] = min(low[u], low[v]);
+        } else if (!sccno[v]) {
+            low[u] = min(low[u], pre[v]);
+        }
+    }
+    if (low[u] == pre[u]) {
+        scc_cnt++;
+        while (1) {
+            int x = S.top(); S.pop();
+            sccno[x] = scc_cnt;
+            if (x == u) break;
+        }
+    }
+}
+
+void find_scc(int n) {
+    dfs_clock = scc_cnt = 0;
+    memset(sccno, 0, sizeof(sccno));
+    memset(pre, 0, sizeof(pre));
+    for (int i = 0; i < n; ++i)
+        if (!pre[i])
+            dfs(i);
+}
